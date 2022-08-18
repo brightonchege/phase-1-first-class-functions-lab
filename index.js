6715b08c8da54951326ff17fefae0150c759fb57
brightonchege <brightonspak@gmail.com>
2022-08-19 00:32:41 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+    let returnFirstTwoDrivers = [...drivers]
+    return  returnFirstTwoDrivers.slice(0,2)
+}
+
+
+function returnLastTwoDrivers(){
+    let returnLastTwoDrivers = [...drivers]
+    return returnLastTwoDrivers.splice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(){
+    return function(num){
+        return num * 5
+    }
+
+}
+
+function fareDoubler(num){
+return num * 2
+}
+
+
+function fareTripler(num){
+    return num * 3
+}
+
+
+
+function selectDifferentDrivers(drivers,Function){
+    if(Function == returnFirstTwoDrivers){
+      return  returnFirstTwoDrivers()
+    }
+    else if(Function == returnLastTwoDrivers){
+     return   returnLastTwoDrivers()
+    }
+}
